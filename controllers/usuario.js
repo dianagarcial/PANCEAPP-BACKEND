3c99f32ee625ad3ae56d50d3f512c3402f84073f
davidsc1509 <david.sanchez_col@uao.edu.co>
2022-11-04 14:39:54 -0500

    Organize este backend pichu

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,14 +1,14 @@
-const {response}=require('express');
+const { response } = require('express');
 const Usuario = require("../models/usuario");
-const bcrypt=require('bcryptjs');
-const {generateJWT} = require('../Helpers/jwt');
-const{RESPONSE_MESSAGES}=require('../Helpers/ResponseMessages');
+const bcrypt = require('bcryptjs');
+const { generateJWT } = require('../Helpers/jwt');
+const { RESPONSE_MESSAGES } = require('../Helpers/ResponseMessages');
 
 const crearUsuario = async (req, res = response) => {
     try {
         let Usuario_ = new Usuario(req.body);
         console.log(Usuario_.contrasena)
-        Usuario_.contrasena=bcrypt.hashSync(Usuario_.contrasena,bcrypt.genSaltSync());
+        Usuario_.contrasena = bcrypt.hashSync(Usuario_.contrasena, bcrypt.genSaltSync());
         await Usuario_.save();
         return res.status(201).json({
             ok: true,
@@ -22,41 +22,44 @@ const crearUsuario = async (req, res = response) => {
         });
     }
 }
-const listUsuario= async(req,res=response)=>{
-    try{
+const listUsuario = async (req, res = response) => {
+    try {
         const Usuario_ = await Usuario.find();
-        if(Usuario_){return res.status(200).json({ok:true,Usuario_});}
+        if (Usuario_) { return res.status(200).json({ ok: true, Usuario_ }); }
         return res.status(404).json({
-            ok:false,
-            msg:RESPONSE_MESSAGES.ERR_NOT_FOUND
+            ok: false,
+            msg: RESPONSE_MESSAGES.ERR_NOT_FOUND
         });
-    }catch(e){
+    } catch (e) {
         return res.status(500).json({
-            ok:false,
-            msg:RESPONSE_MESSAGES.ERR_500});
+            ok: false,
+            msg: RESPONSE_MESSAGES.ERR_500
+        });
     }
 }
-const loginUsuario= async(req,res=response) => {
-    let {correo,contrasena}=req.body;
+const loginUsuario = async (req, res = response) => {
+    let { correo, contrasena } = req.body;
     try {
-     let usuarioDB=await Usuario.findOne({correo});
-     if(!usuarioDB){
-        return res.status(404).json({
-            ok:false,
-            msg:RESPONSE_MESSAGES.ERR_EMAIL_NOT_FOUND
-        })}
-     let validPassword=bcrypt.compareSync(contrasena,usuarioDB.contrasena);
-     if(!validPassword){
-        return res.status(400).json({
-            ok:false,
-            msg:RESPONSE_MESSAGES.ERR_INVALID_PASSWORD
-        })}
-        //const token= await generateJWT(usuarioDB.id,usuarioDB.nombre,usuarioDB.correo,usuarioDB.direccion,usuarioDB.celular);
-    
-     return res.status(200).json({ok:true,_id:usuarioDB})
+        let usuarioDB = await Usuario.findOne({ correo });
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: RESPONSE_MESSAGES.ERR_EMAIL_NOT_FOUND
+            })
+        }
+        let validPassword = bcrypt.compareSync(contrasena, usuarioDB.contrasena);
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: RESPONSE_MESSAGES.ERR_INVALID_PASSWORD
+            })
+        }
+        const token= await generateJWT(usuarioDB.id,usuarioDB.nombre,usuarioDB.correo,usuarioDB.direccion,usuarioDB.celular);
+
+        return res.status(200).json({ ok: true, _id: usuarioDB })
     } catch (error) {
-       
-        return res.status(500).json({ok:false,msg:RESPONSE_MESSAGES.ERR_500})
+
+        return res.status(500).json({ ok: false, msg: RESPONSE_MESSAGES.ERR_500 })
     }
 }
 
