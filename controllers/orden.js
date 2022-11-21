2f87256b835b8901ecb2fadf6e6b3f16dba2e360
Diana Marcela Garcia Lopez <diana_ma.garcia@uao.edu.co>
2022-11-21 09:53:52 -0500

    final pedidos

--- controllers/orden.js
+++ controllers/orden.js
@@ -39,6 +39,17 @@ const listOrdenesPendientes= async(req,res=response)=>{
     }
 }
 
+const listOrdenesFinalizados= async(req,res=response)=>{
+    try{
+        let ordenFinalizado = await Orden.find({estado:1});
+        if(ordenFinalizado){return res.status(200).json({ok:true,ordenFinalizado});}
+        return res.status(404).json({ok:false,msg:RESPONSE_MESSAGES.ERR_NOT_FOUND});
+    }catch(e){
+        logger.error(`readEventos: Internal server error: ${e}`);
+        return res.status(500).json({ok:false,msg:RESPONSE_MESSAGES.ERR_500});
+    }
+}
+
 const listOrden= async(req,res=response)=>{
     try{
        
@@ -60,8 +71,9 @@ const updateOrden = async (req, res = response) => {
     try {
         const orden = await Orden.findById( req.params.idOrden );
         if ( !orden ) {return res.status(404).json({ok: true,msg: RESPONSE_MESSAGES.ERR_NOT_FOUND});}
-        await Orden.updateOne({_id:req.params.idOrden}, {$set:{...req.body}}, { upsert: true });
-        return res.status(200).json({ok: true,msg:RESPONSE_MESSAGES.SUCCESS_2XX})
+        orden.estado=1
+        await orden.save()             
+        return res.status(200).json({ok: true,orden,msg:RESPONSE_MESSAGES.SUCCESS_2XX})
     } catch (e) {
         console.log(`updateRama: Internal server error: ${e}`);
         return res.status(500).json({ok: false,msg: RESPONSE_MESSAGES.ERR_500})
@@ -85,6 +97,7 @@ module.exports = {
     listOrdenes,
     listOrden,
     listOrdenesPendientes,
+    listOrdenesFinalizados,
     updateOrden,
     deleteOrden
 }
--- routes/orden.js
+++ routes/orden.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router();
-const { crearOrden, listOrdenes, listOrden, updateOrden, deleteOrden, listOrdenesPendientes} = require('../controllers/orden');
+const { crearOrden, listOrdenes, listOrden, updateOrden, deleteOrden, listOrdenesPendientes, listOrdenesFinalizados} = require('../controllers/orden');
 
 router.post('/new', crearOrden);
 router.get('/todos', listOrdenes );
 router.get('/pendientes', listOrdenesPendientes );
+router.get('/finalizados', listOrdenesFinalizados );
 
 router.get('/:idOrden', listOrden );
 router.put('/:idOrden',updateOrden)
